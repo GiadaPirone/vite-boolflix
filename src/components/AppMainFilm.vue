b13a9d91f5fe72167d9003fc34279496b0debdf9
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-05-26 16:02:05 +0200

    img film

--- src/components/AppMainFilm.vue
+++ src/components/AppMainFilm.vue
@@ -7,10 +7,19 @@
         data (){
             return{
                 store,
+                urlImg: "https://image.tmdb.org/t/p/",
+                dimensioneImg: "w342",
+                nuovaImg:"https://en.wikipedia.org/wiki/Jellyfish#/media/File:Sea_Nettle_Jelly_1.jpg"
             }
         },
         methods:{
-
+            generaImg(film){
+                if(film.poster_path == null){
+                    return "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5Dy5ec6lBivrOax7FDsoYRR_jtDg2zAUVgQ&usqp=CAU"
+                } else{
+                    return this.store.urlImg + film.poster_path
+                }
+            }
         }
     }
 </script>
@@ -20,6 +29,9 @@
         <h1>Films:</h1>
 
         <div class="card" v-for="film in this.store.Films">
+            <div>
+                <img :src="generaImg(film)" alt="">
+            </div>
             <h2>{{ film.title }}</h2>
             <h3>{{ film.original_title }}</h3>
             <h4>{{ film.original_language }}</h4>
--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import {reactive} from "vue"
 
 export const store= reactive({
+    urlImg: "https://image.tmdb.org/t/p/w185",
     urlSerie:"https://api.themoviedb.org/3/search/tv?api_key=86bfacbb56746e47c21e5ae8bcdeeb10&query=",
     url: "https://api.themoviedb.org/3/search/movie?api_key=86bfacbb56746e47c21e5ae8bcdeeb10&query=",
     Films: [],
